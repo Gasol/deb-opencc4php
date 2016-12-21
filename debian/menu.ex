c19050493f35eef9308e47b64dc4baa2da330e63
Gasol Wu <gasol.wu@gmail.com>
2016-12-21 09:31:26 +0800

    Remove *.ex

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(opencc4php):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="opencc4php" command="/usr/bin/opencc4php"
--- debian/opencc4php.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the opencc4php package
-#
-0 4	* * *	root	[ -x /usr/bin/opencc4php_maintenance ] && /usr/bin/opencc4php_maintenance
